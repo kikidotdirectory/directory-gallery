a5f4435b457366ca54b73eb1caaf5dd52d6a248d
kiki <142045045+calciferchang@users.noreply.github.com>
2026-01-28 23:48:52 -0500

    update license

--- src/cultivate.js
+++ src/cultivate.js
@@ -2,6 +2,10 @@
  * cultivate.js
  * Copyright 2023-2025 Kevin Chen
  *
+ * modified by kiki chang:
+ * - implemented .garden_store.json
+ * - consolidated formal.ejs & natural.ejs into base.ejs
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
